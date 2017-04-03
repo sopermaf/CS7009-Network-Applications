6feee52f192e28e9cc7b665b8edfaf982f8b5cdc
sopermaf <sopermaf@tcd.ie>
2017-04-03 20:08:24 +0100

    searching recursive needs testing for rate limit

--- crawler.py
+++ crawler.py
@@ -1,15 +1,48 @@
 import requests
 import json
+import datetime
 
-destination = "https://api.github.com/users/orangemocha/repos"
-response = requests.get(destination)
-info = json.loads(response.text)
-
-languages = []
-
-for x in range(0, len(info)):
-    languages.append(info[x]['language'])
+def numUniqueLang(languages):
+    uniq = []
+    for lang in languages:
+        if lang not in uniq:
+            uniq.append(lang)
     
-print(languages)
+    print(uniq)
+    return len(uniq)
+
+def userRepoURL(user):
+    return "https://api.github.com/users/" + user + "/repos"
+
+found_users = []
+
+#need to check if I will go over limit of requests
+def wait_check():
+    pass
+
+def searchUser(user):
+    #get repos
+    response1 = requests.get(userRepoURL(user))
+    user_repo_info = json.loads(response1.text)
+    
+    #stats from repos
+    print("CURRENT USER:", user)
+    found_users.append(user)
+    
+    #process repo
+    for repo in user_repo_info:
+        contrib_response = requests.get(repo['contributors_url'])
+        contrib_info = json.loads(contrib_response.text)
+        
+        #process each contributor to that repo
+        for contributor in contrib_info:
+            contrib_username = contributor['login']
+            if contrib_username != user:
+                searchUser(user)
+
+    
+current_user = "GrahamCampbell"
+searchUser(current_user)
+print("LIST OF USERS FOUND:", found_users)
 
 
