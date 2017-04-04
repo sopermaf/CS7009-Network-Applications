c9bc484eb2c5636434598acaf7eb94e28f54eb4f
sopermaf <sopermaf@tcd.ie>
2017-04-04 11:22:38 +0100

    crawling seems to be working from user -> repos -> contribs -> user, wait() implemented as well

--- crawler.py
+++ crawler.py
@@ -1,6 +1,9 @@
 import requests
 import json
 import datetime
+import time
+
+file = open("names.txt", 'w')
 
 def numUniqueLang(languages):
     uniq = []
@@ -18,23 +21,41 @@ found_users = []
 
 #need to check if I will go over limit of requests
 def wait_check():
-    pass
-
+    get_rate = "https://api.github.com/rate_limit"
+    wait_info = json.loads(requests.get(get_rate).text)
+    req_remaining = wait_info['resources']['core']['remaining']
+    req_remaining = int(req_remaining)
+    
+    if req_remaining > 5:
+        return
+    
+    reset = wait_info['resources']['core']['reset']
+    time_reset = datetime.datetime.fromtimestamp(reset)
+    wait_time_sec = reset - time.time()
+    
+    print("\nSleeping for", round(wait_time_sec/60), "mins...")
+    print("Waking at", time_reset)
+    time.sleep(wait_time_sec)
+    print("Sleep ended..\n")
+    
 def searchUser(user):
     #get repos
-    response1 = requests.get(userRepoURL(user))
+    wait_check()
+    response1 = requests.get(userRepoURL(user))             #request 1
     user_repo_info = json.loads(response1.text)
     
-    #stats from repos
+    #stats from user
     print("CURRENT USER:", user)
     found_users.append(user)
+    file.write(user + "\n")             #write stats to file
     
-    #process repo
+    #process repos
     for repo in user_repo_info:
-        contrib_response = requests.get(repo['contributors_url'])
+    
+        #access the list of contributors for that repo
+        wait_check()
+        contrib_response = requests.get(repo['contributors_url'])       #request 2
         contrib_info = json.loads(contrib_response.text)
-        
-        #process each contributor to that repo
         for contributor in contrib_info:
             contrib_username = contributor['login']
             if contrib_username != user:
@@ -45,4 +66,6 @@ current_user = "GrahamCampbell"
 searchUser(current_user)
 print("LIST OF USERS FOUND:", found_users)
 
+#wait_check()
 
+#file.close()
--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-import requests
-import json
 
+file = open("test.txt", 'w')
+
+i = 0
+while True:
+    file.write(str(i)+'\n')
+    i += 1
+    print(file)
+    input("press enter")
+    
+file.close()
